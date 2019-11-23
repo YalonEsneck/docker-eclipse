9b53e0bc22c8c469d709337d07ed977f7ea4697e
Jan Merkelbag <yalon.esneck@gmail.com>
2019-11-23 15:38:25 +0100

    adds installer script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -u
+ROOT_DIR=$(realpath $(dirname ${0}))
+source ${ROOT_DIR}/facts.sh
+
+# Ensure that the tmp files directory exists for the application's icon
+mkdir -p ${FILES_TMP_DIR}
+
+# Get the icon file and dump it into tmp files directory
+containerId=$(docker create ${DOCKER_IMAGE})
+docker cp ${containerId}:/opt/eclipse/icon.xpm ${FILES_TMP_DIR}/${DOCKER_TAG}.xpm
+docker rm -f ${containerId} > /dev/null
+
+cat <<EOF > "${HOME}/.local/share/applications/eclipse-${DOCKER_TAG}.desktop"
+[Desktop Entry]
+Name=Eclipse - ${DOCKER_TAG}
+Type=Application
+Exec=${ROOT_DIR}/run.sh
+Terminal=false
+Icon=${FILES_TMP_DIR}/${DOCKER_TAG}.xpm
+Comment=Integrated Development Environment
+NoDisplay=false
+Categories=Development;IDE
+Name[en]=Eclipse - ${DOCKER_TAG}
+EOF
